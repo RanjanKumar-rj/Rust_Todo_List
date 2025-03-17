35805768ac1a21d383bb871589359c1c8f358f85
Ranjan Kumar <kumar.ranjan@sasken.com>
2025-03-17 23:08:31 +0530

    Added loop so that user can add any number of Todo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Todo App");
+    loop{
+        println!("Enter your choice");
+        println!("1. Add Todo");
+        println!("2. List Todo");
+        println!("3. Exit");
+        break;
+    }
 }
